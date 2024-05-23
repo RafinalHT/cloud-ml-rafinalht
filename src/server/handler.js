6b02e6ec68e50547b2ed9319654b8113141696c1
rafinal.haryokusumo <rafinal.haryokusumo@ui.ac.id>
2024-05-23 19:59:15 +0700

    bangkit

---
+++ src/server/handler.js
@@ -0,0 +1,51 @@
+const predictClassification = require("../services/inferenceService");
+const crypto = require("crypto");
+const storeData = require("../services/storeData");
+const { v4: uuid } = require("uuid");
+
+async function postPredictHandler(request, h) {
+  const { image } = request.payload;
+  const { model } = request.server.app;
+
+  try {
+    const predictions = await predictClassification(model, image);
+    const [cancer] = predictions;
+
+    const id = uuid();
+    const createdAt = new Date().toISOString();
+
+    if (cancer === 1) {
+      result = "Cancer";
+      suggestion = "Segera periksa ke dokter!";
+    } else {
+      result = "Non-cancer";
+      suggestion = "Tidak ada cancer";
+    }
+
+    const data = {
+      id: id,
+      result: result,
+      suggestion: suggestion,
+      createdAt: createdAt,
+    };
+
+    await storeData(id, data);
+
+    return h
+      .response({
+        status: "success",
+        message: "Model is predicted successfully",
+        data: data,
+      })
+      .code(201);
+  } catch (error) {
+    return h
+      .response({
+        status: "fail",
+        message: "Terjadi kesalahan dalam melakukan prediksi",
+      })
+      .code(400);
+  }
+}
+
+module.exports = postPredictHandler;
